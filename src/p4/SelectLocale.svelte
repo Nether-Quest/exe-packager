866d86a1d0a6c606e0a5e51975a201b786f0783b
FerrariBulletA380 <119022143+FerrariBulletA380@users.noreply.github.com>
2022-11-27 13:03:04 +0800

    Delete src directory

--- src/p4/SelectLocale.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import {locale, supportedLocales} from '../locales/index';
-</script>
-
-<select bind:value={$locale}>
-  {#each Object.entries(supportedLocales) as [locale, name]}
-    <option value={locale}>{name}</option>
-  {/each}
-</select>
